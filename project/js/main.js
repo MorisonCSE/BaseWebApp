8f6cbd10f34e7fb5ae2c6190da2199fc27096b98
Alex Morison <morison.alex@gmail.com>
2024-05-02 22:20:11 -0400

    fixed city and temp references

--- project/js/main.js
+++ project/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 function getWeather() {
   var url = "https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&appid=" + apiKey;
   $.ajax(url, {success: function(data) {
-    $("city").text(data.name);
-    $("temp").text(data.main.temp);
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
   }})
 }
