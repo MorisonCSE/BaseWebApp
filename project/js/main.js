776c9eb75a6415ff124c8c4817b283644623c9f3
Alex Morison <morison.alex@gmail.com>
2024-05-02 21:53:03 -0400

    added open weather api call.

--- project/js/main.js
+++ project/js/main.js
@@ -3,7 +3,16 @@ function showPicture(){
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
   $("p").html("High five! You're building your first web app!");
+}
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+// jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
+$(document).ready(function() {
+  getWeather();
+}) 
+
+function getWeather() {
+  var url = "https://api.openweathermap.org/data/2.5/weather?q={boston}&appid=" + apiKey;
+  $.ajax(url, {success: function(data) {
+    console.log(data);
+  }})
 }
